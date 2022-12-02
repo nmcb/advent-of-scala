2ef43ed9ff29303060fae91487497acaff148082
nmcb <marco@splatter.nl>
2022-12-02 08:42:39 +0000

    solution day 2

--- src/test/scala/TestSolutions.scala
+++ src/test/scala/TestSolutions.scala
@@ -6,3 +6,7 @@ class TestSolutions extends AnyFunSuite:
     assertResult( 69501)(actual = Day01.answer1)
     assertResult(202346)(actual = Day01.answer2)
   }
+  test("Day02 [1ms]") {
+    assertResult(11449)(actual = Day02.answer1)
+    assertResult(13187)(actual = Day02.answer2)
+  }
