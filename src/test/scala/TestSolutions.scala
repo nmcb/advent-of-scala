225d85146220a272b5c5c08f1944ed222ce3da68
nmcb <marco@splatter.nl>
2022-12-04 06:24:09 +0100

    solved day 4

--- src/test/scala/TestSolutions.scala
+++ src/test/scala/TestSolutions.scala
@@ -2,15 +2,19 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestSolutions extends AnyFunSuite:
 
-  test("Day01 [1ms]") {
+  test("Day01 [32ms]") {
     assertResult( 69501)(actual = Day01.answer1)
     assertResult(202346)(actual = Day01.answer2)
   }
-  test("Day02 [1ms]") {
+  test("Day02 [11ms]") {
     assertResult(11449)(actual = Day02.answer1)
     assertResult(13187)(actual = Day02.answer2)
   }
-  test("Day03 [1ms]") {
+  test("Day03 [43ms]") {
     assertResult(8139)(actual = Day03.answer1)
     assertResult(2668)(actual = Day03.answer2)
+  }
+  test("Day04 [10ms]") {
+    assertResult(651)(actual = Day04.answer1)
+    assertResult(956)(actual = Day04.answer2)
   }
