7ab798046f6d067d0ef01655dc34ff9657da3035
nmcb <marco@splatter.nl>
2022-12-20 11:30:50 +0100

    solved day 18

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -92,48 +92,48 @@ object Day17 extends App:
 
   /** Utilities */
 
-  def brent(f: Int => Int, x0: Int): (Int, Int) = {
-    val lambda = findLambda(f, x0)
-    val mu = findMu(f, x0, lambda)
-    (lambda, mu)
-  }
-
-  def cycle(f: Int => Int, x0: Int): Seq[Int] = {
-    val (lambda, mu) = brent(f, x0)
-    (1 until mu + lambda)
-      .foldLeft(Seq(x0))((list, _) => f(list.head) +: list)
-      .reverse
-      .drop(mu)
-  }
-
-  def findLambda(f: Int => Int, x0: Int): Int = {
-    findLambdaRec(f, tortoise = x0, hare = f(x0), power = 1, lambda = 1)
-  }
-
-  def findMu(f: Int => Int, x0: Int, lambda: Int): Int = {
-    val hare = (0 until lambda).foldLeft(x0)((x, _) => f(x))
-    findMuRec(f, tortoise = x0, hare, mu = 0)
-  }
-
-  @tailrec
-  private def findLambdaRec(f: Int => Int, tortoise: Int, hare: Int, power: Int, lambda: Int): Int = {
-    if (tortoise == hare) {
-      lambda
-    } else {
-      val (newTortoise, newPower, newLambda) = if (power == lambda) {
-        (hare, power * 2, 0)
-      } else {
-        (tortoise, power, lambda)
-      }
-      findLambdaRec(f, newTortoise, f(hare), newPower, newLambda + 1)
-    }
-  }
-
-  @tailrec
-  private def findMuRec(f: Int => Int, tortoise: Int, hare: Int, mu: Int): Int = {
-    if (tortoise == hare) {
-      mu
-    } else {
-      findMuRec(f, f(tortoise), f(hare), mu + 1)
-    }
-  }
+//  def brent[A](f: A => A, z: A): (A, A) = {
+//    val lambda = findLambda(f, z)
+//    val mu = findMu(f, z, lambda)
+//    (lambda, mu)
+//  }
+//
+//  def cycle[A](f: A => A, z: A)(given unit: Monoid[A]): Seq[A] = {
+//    val (lambda, mu) = brent(f, z)
+//    (unit.Z until mu + lambda)
+//      .foldLeft(Seq(z))((list, _) => f(list.head) +: list)
+//      .reverse
+//      .drop(mu)
+//  }
+//
+//  def findLambda(f: Int => Int, x0: Int): Int = {
+//    findLambdaRec(f, tortoise = x0, hare = f(x0), power = 1, lambda = 1)
+//  }
+//
+//  def findMu(f: Int => Int, x0: Int, lambda: Int): Int = {
+//    val hare = (0 until lambda).foldLeft(x0)((x, _) => f(x))
+//    findMuRec(f, tortoise = x0, hare, mu = 0)
+//  }
+//
+//  @tailrec
+//  private def findLambdaRec(f: Int => Int, tortoise: Int, hare: Int, power: Int, lambda: Int): Int = {
+//    if (tortoise == hare) {
+//      lambda
+//    } else {
+//      val (newTortoise, newPower, newLambda) = if (power == lambda) {
+//        (hare, power * 2, 0)
+//      } else {
+//        (tortoise, power, lambda)
+//      }
+//      findLambdaRec(f, newTortoise, f(hare), newPower, newLambda + 1)
+//    }
+//  }
+//
+//  @tailrec
+//  private def findMuRec(f: Int => Int, tortoise: Int, hare: Int, mu: Int): Int = {
+//    if (tortoise == hare) {
+//      mu
+//    } else {
+//      findMuRec(f, f(tortoise), f(hare), mu + 1)
+//    }
+//  }
---
+++ src/main/scala/Day19.scala
@@ -0,0 +1,155 @@
+import scala.io.*
+import Robot.*
+import Material.*
+
+enum Material:
+  case Ore
+  case Clay
+  case Obsidian
+  case Geode
+
+case class Robot(material: Material)
+
+case class Blueprint( index: Int, oreRobotCost: Int, clayRobotCost: Int, obsidianRobotCostOre: Int, obsidianRobotCostClay: Int, geodeRobotCostOre: Int, geodeRobotCostObsidian: Int):
+  val highestOreCost = List(oreRobotCost, clayRobotCost, obsidianRobotCostOre, geodeRobotCostOre).max
+
+case class MaterialStash(ore: Int = 0, clay: Int = 0, obsidian: Int = 0, geode: Int = 0)
+
+sealed trait Action
+case object DoNothing extends Action
+case object BuildOreRobot extends Action
+case object BuildClayRobot extends Action
+case object BuildObsidianRobot extends Action
+case object BuildRobotGeode extends Action
+
+def parseBlueprint(s: String): Blueprint =
+  s match {
+    case s"Blueprint ${index}: Each ore robot costs ${oreRobotCost} ore. Each clay robot costs ${clayRobotCost} ore. Each obsidian robot costs ${obsidianRobotCostOre} ore and ${obsidianRobotCostClay} clay. Each geode robot costs ${geodeRobotCostOre} ore and ${geodeRobotCostObsidian} obsidian." => Blueprint(index.toInt, oreRobotCost.toInt, clayRobotCost.toInt, obsidianRobotCostOre.toInt, obsidianRobotCostClay.toInt, geodeRobotCostOre.toInt, geodeRobotCostObsidian.toInt)
+    case _ => sys.error("booboo")
+  }
+
+def checkBuildRobot(blueprint: Blueprint, material: MaterialStash, robots: List[Robot]): List[Action] =
+  val curOre = material.ore
+  val curClay = material.clay
+  val curObsidian = material.obsidian
+
+  def count(robot: Robot): Int =
+    robots.count(_ == robot)
+
+  val oreRobot: Option[Action] =
+    if curOre >= blueprint.oreRobotCost && count(Robot(Ore)) <= blueprint.highestOreCost then
+      Some(BuildOreRobot)
+    else
+      None
+  val clayRobot: Option[Action] =
+    if curOre >= blueprint.clayRobotCost && count(Robot(Clay)) <= blueprint.obsidianRobotCostClay then
+      Some(BuildClayRobot)
+    else
+      None
+  val obsidianRobot: Option[Action] =
+    if curOre >= blueprint.obsidianRobotCostOre && curClay >= blueprint.obsidianRobotCostClay then
+      Some(BuildObsidianRobot)
+    else
+      None
+  val geodeRobot: Option[Action] =
+    if curOre >= blueprint.geodeRobotCostOre && curObsidian >= blueprint.geodeRobotCostObsidian then
+      Some(BuildRobotGeode)
+    else
+      None
+
+  if material.obsidian >= blueprint.geodeRobotCostObsidian then
+    List(geodeRobot).flatten
+  else if material.clay >= blueprint.obsidianRobotCostClay then
+    List(geodeRobot, obsidianRobot, Some(DoNothing)).flatten
+  else if material.ore >= blueprint.highestOreCost then
+    List(oreRobot, clayRobot, obsidianRobot, geodeRobot).flatten
+  else
+    List(oreRobot, clayRobot, obsidianRobot, geodeRobot, Some(DoNothing)).flatten
+
+
+def buildRobot(action: Action): Robot =
+  action match {
+    case BuildOreRobot      => Robot(Ore)
+    case BuildClayRobot     => Robot(Clay)
+    case BuildObsidianRobot => Robot(Obsidian)
+    case BuildRobotGeode    => Robot(Geode)
+    case _ => sys.error("boom robot!")
+  }
+
+def produceMaterial(robots: List[Robot], stash: MaterialStash): MaterialStash = {
+  robots match {
+    case h :: t => h match {
+      case Robot(Ore)       => produceMaterial(t, stash.copy(ore = stash.ore + 1))
+      case Robot(Clay)      => produceMaterial(t, stash.copy(clay = stash.clay + 1))
+      case Robot(Obsidian)  => produceMaterial(t, stash.copy(obsidian = stash.obsidian + 1))
+      case Robot(Geode)     => produceMaterial(t, stash.copy(geode = stash.geode + 1))
+    }
+    case Nil => stash
+  }
+}
+
+def consumeMaterial(action: Action, blueprint: Blueprint, stash: MaterialStash): MaterialStash = {
+  action match {
+    case BuildOreRobot      => stash.copy(ore = stash.ore - blueprint.oreRobotCost)
+    case BuildClayRobot     => stash.copy(ore = stash.ore - blueprint.clayRobotCost)
+    case BuildObsidianRobot => stash.copy(ore = stash.ore - blueprint.obsidianRobotCostOre, clay = stash.clay - blueprint.obsidianRobotCostClay)
+    case BuildRobotGeode    => stash.copy(ore = stash.ore - blueprint.geodeRobotCostOre, obsidian = stash.obsidian - blueprint.geodeRobotCostObsidian)
+    case _ => sys.error("boom robot!")
+  }
+}
+
+def geodeCollectionFlows(blueprint: Blueprint, material: MaterialStash, robots: List[Robot], timeLimit: Int, curMax: Int, currentTime: Int = 1): Int =
+
+  val newMax =
+    if currentTime == timeLimit && material.geode + robots.count(_ == Robot(Geode)) > curMax then
+      material.geode + robots.count(_ == Robot(Geode))
+    else
+      curMax
+
+  if currentTime == timeLimit then
+    material.geode + robots.count(_ == Robot(Geode))
+  else
+    val newActions = checkBuildRobot(blueprint, material, robots)
+    newActions.foldLeft(newMax)((max, action) =>
+      if action == DoNothing then
+        val newMaterial = produceMaterial(robots, material)
+        val possibleScore = geodeCollectionFlows(blueprint, newMaterial, robots, timeLimit, newMax, currentTime + 1)
+        if (possibleScore > max) {possibleScore} else max
+      else
+        val newRobot = buildRobot(action)
+        val newMaterial = consumeMaterial(action, blueprint, produceMaterial(robots, material))
+        val possibleScore = geodeCollectionFlows(blueprint, newMaterial, newRobot :: robots, timeLimit, newMax, currentTime + 1)
+        if (possibleScore > max) {possibleScore} else max
+    )
+
+object Day19 extends App:
+
+  val day: String =
+    this.getClass.getName.drop(3).init
+
+
+  val input =
+    Source.fromResource(s"input$day.txt").getLines.map(parseBlueprint).toList
+
+  val start1: Long =
+    System.currentTimeMillis
+
+  val answer1: Int =
+    input
+      .map(x =>
+        geodeCollectionFlows(x, MaterialStash(), List(Robot(Ore)), 24, 0) * x.index
+      ).sum
+
+  println(s"Answer day $day part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
+
+  val start2: Long =
+    System.currentTimeMillis
+
+  val answer2: Int =
+    input
+      .take(3)
+      .map(x =>
+        geodeCollectionFlows(x, MaterialStash(), List(Robot(Ore)), 32, 0)
+      ).product
+
+  println(s"Answer day $day part 2: $answer2 [${System.currentTimeMillis - start1}ms]")
