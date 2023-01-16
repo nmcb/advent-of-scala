41eaf128e42944fa8f4ff7149b9898fa730e91a9
nmcb <marco@splatter.nl>
2023-01-16 13:57:19 +0100

    implemented cycle detection

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -1,6 +1,8 @@
 import scala.annotation.*
+import scala.collection.AbstractIterator
 import scala.io.*
 import scala.math.*
+import scala.math.Integral.Implicits.*
 
 object Day17 extends App:
 
@@ -13,23 +15,27 @@ object Day17 extends App:
       .trim
       .toList
 
-  val pattern: LazyList[Move] =
-      input.to(LazyList) #::: pattern
-
   case class Pos(x: Int, y: Int):
+    def -(that: Pos): Pos = Pos(x - that.x, y - that.y)
     def translate(dx: Int, dy: Int): Pos = Pos(x + dx, y + dy)
 
   object Pos:
     val origin: Pos = Pos(0,0)
+
 
   type Move = Char
   val L = '<'
   val R = '>'
   val D = 'v'
 
+  object Move:
+
+    val pattern: LazyList[Move] =
+      input.to(LazyList) #::: pattern
+
+
   sealed abstract class Rock(val relative: List[Pos]):
-    def withOrigin(o: Pos): List[Pos] =
-      relative.map(p => Pos(p.x + o.x, p.y + o.y))
+    def withOrigin(o: Pos): List[Pos] = relative.map(p => Pos(p.x + o.x, p.y + o.y))
 
   case object Min   extends Rock(List(Pos(0,0),Pos(1,0),Pos(2,0),Pos(3,0)))
   case object Plus  extends Rock(List(Pos(1,0),Pos(0,1),Pos(1,1),Pos(2,1),Pos(1,2)))
@@ -37,175 +43,155 @@ object Day17 extends App:
   case object Stack extends Rock(List(Pos(0,0),Pos(0,1),Pos(0,2),Pos(0,3)))
   case object Box   extends Rock(List(Pos(0,0),Pos(1,0),Pos(0,1),Pos(1,1)))
 
-  object Rocks:
-    val rocks: LazyList[Rock] = LazyList(Min, Plus, El, Stack, Box) #::: rocks
+  object Rock:
+    val sequence: LazyList[Rock] = LazyList(Min, Plus, El, Stack, Box) #::: sequence
 
-  var rockCount: Int = 0
 
-  case class Chamber(pattern: LazyList[Move], stopped: Vector[(Pos,Rock)]):
-    def width: Int = 7
-    def height: Int = if isEmpty then Pos.origin.y else tower.map(_.y).max + 1
-    def tower: Vector[Pos] = stopped.take(100).flatMap((p,r) => r.withOrigin(p))
-    def isWall(p: Pos): Boolean     = p.x < Pos.origin.x || p.x >= Pos.origin.x + width
+  case class Chamber(rocks: LazyList[Rock], pattern: LazyList[Move], dropped: Set[Pos], height: Int):
+    import Chamber.*
+
+    def isWall(p: Pos): Boolean     = p.x < Pos.origin.x | p.x >= Pos.origin.x + width
     def isFloor(p: Pos): Boolean    = p.y < Pos.origin.y
-    def isOccupied(p: Pos): Boolean = isWall(p) || isFloor(p) || tower.contains(p)
-    def isEmpty: Boolean = stopped.isEmpty
+    def isOccupied(p: Pos): Boolean = isWall(p) | isFloor(p) | dropped.contains(p)
 
-    def next(rock: Rock): Chamber =
+    def next: Chamber =
 
-      def appear: Pos = Pos.origin.translate(dx = 2, dy = height + 3)
+      def appear: Pos =
+        Pos.origin.translate(dx = 2, dy = height + 3)
 
-      def loop(p: Pos, moves: LazyList[Move], a: List[(Move,Option[Pos])] = List.empty): List[(Move,Option[Pos])] =
-        val (m,n) = moves.head match
-          case L => (L, p.translate(dx = -1, dy =  0))
-          case R => (R, p.translate(dx =  1, dy =  0))
-          case D => (D, p.translate(dx =  0, dy = -1))
+      type Trace = List[(Move,Option[Pos])]
 
-        if rock.withOrigin(n).forall(p => !isOccupied(p)) then
-          if m == L || m == R then
-            loop(n, D #:: moves.tail, (m, Some(n)) :: a)
+      val trace: Trace =
+        def drop(p: Pos, moves: LazyList[Move], trace: Trace = List.empty): Trace =
+          val (m,n) = moves.head match
+            case L => (L, p.translate(dx = -1, dy =  0))
+            case R => (R, p.translate(dx =  1, dy =  0))
+            case D => (D, p.translate(dx =  0, dy = -1))
+
+          if rocks.head.withOrigin(n).forall(p => !isOccupied(p)) then
+            if m == L || m == R then
+              drop(n, D #:: moves.tail, (m, Some(n)) :: trace)
+            else
+              drop(n, moves.tail, (m, Some(n)) :: trace)
           else
-            loop(n, moves.tail, (m, Some(n)) :: a)
-        else
-          if m == L || m == R then
-            loop(p, D #:: moves.tail, (m, None) :: a)
-          else
-            a
+            if m == L || m == R then
+              drop(p, D #:: moves.tail, (m, None) :: trace)
+            else
+              trace
 
-      val ms = loop(appear, pattern)
-      val ns =
-        ms.dropWhile((_,op) => op.isEmpty)
+        drop(appear, pattern)
+
+      val halted: Set[Pos] =
+        trace.dropWhile((_,op) => op.isEmpty)
           .headOption
-          .flatMap((_,op) => op.map(p => (p,rock) +: stopped))
-          .getOrElse(stopped)
-      val np = pattern.drop(ms.filterNot((m,_) => m == D).size)
-      if ms.nonEmpty then Chamber(pattern = np, stopped = ns) else this
+          .flatMap((_,op) => op.map(p => dropped ++ rocks.head.withOrigin(p)))
+          .getOrElse(dropped)
+
+      val maxY: Int =
+        halted.map(_.y).max + 1
+
+      val consumed: LazyList[Move] =
+        pattern.drop(trace.filterNot((m,_) => m == D).size)
+
+      Chamber(rocks = rocks.tail, pattern = consumed, dropped = halted.filter(_.y >= maxY - slidingWindow), height = maxY)
+
+    def cycleInvariant: Set[Pos] =
+      dropped.map(_ - Pos(0, height - slidingWindow))
+
 
   object Chamber:
-    def empty: Chamber =
-      Chamber(pattern = pattern, stopped = Vector.empty)
 
-  val start1: Long = System.currentTimeMillis
-  val answer1: Int = Rocks.rocks.take(2022).foldLeft(Chamber.empty)(_ next _).height
+    val slidingWindow: Int =
+      100
+
+    val width: Int =
+      7
+
+    def empty: Chamber =
+      Chamber(Rock.sequence, Move.pattern, Set.empty, 0)
+
+
+  val start1: Long =
+    System.currentTimeMillis
+
+  val answer1: Int =
+    Iterator.iterate(Chamber.empty)(_.next).drop(2022).next.height
+
   println(s"Answer day $day part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
 
 
-  type Nr = Int
-  val nr: Nr = 10000
-  val stack: IndexedSeq[(Pos,Rock)] =
-    Rocks
-      .rocks
-      .take(nr)
-      .foldLeft(Chamber.empty)(_ next _)
-      .stopped
-      .reverse
+  val start2: Long =
+    System.currentTimeMillis
 
-  case class RocksBy(underlying: IndexedSeq[(Pos,Rock)]):
-    def row(y: Int): IndexedSeq[Option[(Rock,Nr)]] =
-      val xToRockNrOnY: Map[Int,(Rock,Nr)] =
-        underlying
-          .zipWithIndex
-          .map{ case (or,i) => or -> i }
-          .flatMap{ case ((o,r),nr) => r.withOrigin(o).map(p => p -> (r, nr)) }
-          .filter{ case (p,_) => p.y == y }
-          .map((p,rnr) => p.x -> rnr)
-          .toMap
-      (0 until 7).foldLeft(Vector.empty)((a,x) => {
-        a :+ xToRockNrOnY.get(x).orElse(None)
-      })
+  val answer2: Long =
 
-  def floors: List[Int] =
-    stack
-      .flatMap((p,r) => r.withOrigin(p))
-      .groupMap(_.y)(_.x)
-      .filter(_._2.size == 7)
-      .map(_._1)
-      .toList
-      .sorted
+    val cycle: Cycle[Chamber] =
+      CycleFinder.find(Chamber.empty, _.next)(_.cycleInvariant)
 
-  def diff: List[Int] =
-    floors.zip(floors.tail).map((f1,f2) => f2 - f1)
+    val (cycleNr, tailNr) =
+      (1000_000_000_000L - cycle.stemLength) /% cycle.cycleLength
 
-  /** Cycle Detected - Manually - By Human Bot */
+    val stemHeight: Long =
+      cycle.cycleHead.height
 
-  def startY: Int =
-    655
+    val cycleHeight: Long =
+      cycle.cycleHeadRepeat.height - stemHeight
 
-  def cycleY: Int =
-    101+237+32+280+56+197+226+126+6+30+18+127+113+483+20+407+129+97
+    val tailHeight: Long =
+      Iterator.iterate(cycle.cycleHead)(_.next).drop(tailNr.toInt).next.height - stemHeight
 
-  def startNr: Int =
-    RocksBy(stack).row(startY).flatMap(_.map(_._2)).max
+    stemHeight + cycleNr * cycleHeight + tailHeight
 
-  def cycleNr: Int =
-    RocksBy(stack).row(startY + cycleY).flatMap(_.map(_._2)).max - startNr
-
-  def totalNr: Long =
-    1000_000_000_000
-
-  def restNr: Long =
-    (totalNr - startNr) % cycleNr
-
-  def cycleSize: Long =
-    (totalNr - startNr - restNr) / cycleNr
-
-  def endY: Long =
-    startY + cycleY * cycleSize
-
-  def deltaY: Long =
-    Rocks.rocks.take((startNr + restNr).toInt).foldLeft(Chamber.empty)(_ next _).height - startY
-
-  def totalY: Long =
-    endY + deltaY
-
-  val start2: Long = System.currentTimeMillis
-  val answer2 = totalY
   println(s"Answer day $day part 2: $answer2 [${System.currentTimeMillis - start2}ms]")
+
 
   /** Utilities */
 
-//  def brent[A](f: A => A, z: A): (A, A) = {
-//    val lambda = findLambda(f, z)
-//    val mu = findMu(f, z, lambda)
-//    (lambda, mu)
-//  }
-//
-//  def cycle[A](f: A => A, z: A)(given unit: Monoid[A]): Seq[A] = {
-//    val (lambda, mu) = brent(f, z)
-//    (unit.Z until mu + lambda)
-//      .foldLeft(Seq(z))((list, _) => f(list.head) +: list)
-//      .reverse
-//      .drop(mu)
-//  }
-//
-//  def findLambda(f: Int => Int, x0: Int): Int = {
-//    findLambdaRec(f, tortoise = x0, hare = f(x0), power = 1, lambda = 1)
-//  }
-//
-//  def findMu(f: Int => Int, x0: Int, lambda: Int): Int = {
-//    val hare = (0 until lambda).foldLeft(x0)((x, _) => f(x))
-//    findMuRec(f, tortoise = x0, hare, mu = 0)
-//  }
-//
-//  @tailrec
-//  private def findLambdaRec(f: Int => Int, tortoise: Int, hare: Int, power: Int, lambda: Int): Int = {
-//    if (tortoise == hare) {
-//      lambda
-//    } else {
-//      val (newTortoise, newPower, newLambda) = if (power == lambda) {
-//        (hare, power * 2, 0)
-//      } else {
-//        (tortoise, power, lambda)
-//      }
-//      findLambdaRec(f, newTortoise, f(hare), newPower, newLambda + 1)
-//    }
-//  }
-//
-//  @tailrec
-//  private def findMuRec(f: Int => Int, tortoise: Int, hare: Int, mu: Int): Int = {
-//    if (tortoise == hare) {
-//      mu
-//    } else {
-//      findMuRec(f, f(tortoise), f(hare), mu + 1)
-//    }
-//  }
+  case class Cycle[A](stemLength: Int, cycleLength: Int, cycleHead: A, cycleLast: A, cycleHeadRepeat: A)
+
+  object CycleFinder:
+
+    import scala.collection._
+
+    extension [A](it: Iterator[A]) def zipWithPrev: Iterator[(Option[A], A)] =
+        new AbstractIterator[(Option[A], A)]:
+
+          private var prevOption: Option[A] =
+            None
+
+          override def hasNext: Boolean =
+            it.hasNext
+
+          override def next: (Option[A], A) =
+            val cur = it.next
+            val ret = (prevOption, cur)
+            prevOption = Some(cur)
+            ret
+
+    def find[A, B](coll: IterableOnce[A])(m: A => B): Option[Cycle[A]] =
+
+      val trace: mutable.Map[B, (A, Int)] =
+        mutable.Map[B, (A, Int)]()
+
+      coll.iterator
+        .zipWithPrev
+        .zipWithIndex
+        .map({ case ((last, prev), idx) => (last, prev, trace.put(m(prev), (prev, idx)), idx) })
+        .collectFirst({ case (Some(last), repeat, Some((prev, prevIdx)), idx) =>
+          Cycle(
+            stemLength      = prevIdx,
+            cycleLength     = idx - prevIdx,
+            cycleHead       = prev,
+            cycleLast       = last,
+            cycleHeadRepeat = repeat
+          )
+        })
+
+    def find[A, B](x0: A, f: A => A)(m: A => B): Cycle[A] =
+      find(Iterator.iterate(x0)(f))(m).get
+
+
+
+
+
+
