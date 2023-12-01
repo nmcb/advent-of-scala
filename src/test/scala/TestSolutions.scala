6c35e2750f003d8c2d67f151fe568613d88f213e
nmcb <nmcb@xs4all.nl>
2023-12-01 08:33:37 +0100

    baselined

--- src/test/scala/TestSolutions.scala
+++ src/test/scala/TestSolutions.scala
@@ -2,7 +2,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestSolutions extends AnyFunSuite:
 
-  test("Day01 [266ms]") {
+  test("Day01") {
     assertResult(54916)(actual = Day01.answer1)
     assertResult(54728)(actual = Day01.answer2)
   }
