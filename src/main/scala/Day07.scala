dea1ba1b2f881351538d15c0360954f55819ed20
nmcb <nmcb@xs4all.nl>
2023-12-06 07:16:01 +0100

    baselining

---
+++ src/main/scala/Day07.scala
@@ -0,0 +1,23 @@
+import scala.annotation.tailrec
+import scala.io.*
+
+object Day07 extends App:
+
+  val day: String =
+    this.getClass.getName.drop(3).init
+
+  val start1: Long =
+    System.currentTimeMillis
+
+  lazy val answer1: Long =
+    ???
+
+  println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
+
+  val start2: Long =
+    System.currentTimeMillis
+
+  lazy val answer2: Long =
+    ???
+
+  println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
