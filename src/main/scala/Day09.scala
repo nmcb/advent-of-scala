7a1e00424b466444fc6db156a36a99664875f473
nmcb <nmcb@xs4all.nl>
2023-12-09 10:41:09 +0100

    incorporated jp's solution

--- src/main/scala/Day09.scala
+++ src/main/scala/Day09.scala
@@ -1,33 +1,26 @@
-import scala.annotation.tailrec
+import  scala.annotation.tailrec
 import scala.io.*
 import scala.math.Ordered.orderingToOrdered
 
 object Day09 extends App:
 
-  val day: String =
-    this.getClass.getName.drop(3).init
+  val day: String = this.getClass.getName.drop(3).init
 
-  type Input = Vector[Vector[Int]]
-
-  lazy val input: Input =
+  lazy val input: Vector[Vector[Int]] =
     Source
       .fromResource(s"input$day.txt")
       .getLines
       .map(_.split(' ').map(_.toInt).toVector)
       .toVector
 
-  type Triangle = Vector[Vector[Int]]
-
-  def triangle(numbers: Vector[Int], acc: Triangle = Vector.empty): Triangle =
-    if numbers.forall(_ == 0) then acc :+ numbers else triangle(numbers.tail.zip(numbers).map(_-_), acc :+ numbers)
-
-  def solve(input: Input)(extrapolation: Triangle => Int): Int =
-    input.map(numbers => triangle(numbers)).map(extrapolation).sum
+  def extrapolate(line: Vector[Int]): Int =
+    val diffs = line.tail.zip(line).map(_-_)
+    if diffs.forall(_ == 0L) then line.last else line.last + extrapolate(diffs)
 
   val start1: Long = System.currentTimeMillis
-  val answer1: Int = solve(input)(_.map(_.last).foldRight(0)(_+_))
+  val answer1: Int = input.map(extrapolate).sum
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
   val start2: Long = System.currentTimeMillis
-  val answer2: Int = solve(input)(_.map(_.head).foldRight(0)(_-_))
+  val answer2: Int = input.map(_.reverse).map(extrapolate).sum
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
