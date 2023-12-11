6b291ac09b005c34afaba4aa5aeff0745c449103
nmcb <nmcb@xs4all.nl>
2023-12-11 13:14:49 +0100

    baselining day 12

---
+++ src/main/scala/Day12.scala
@@ -0,0 +1,21 @@
+import scala.annotation.tailrec
+import scala.io.*
+import scala.math.Ordered.orderingToOrdered
+
+object Day12 extends App:
+
+  val day: String = this.getClass.getName.drop(3).init
+
+  lazy val input: Vector[String] =
+    Source
+      .fromResource(s"input$day.txt")
+      .getLines
+      .toVector
+
+  val start1: Long = System.currentTimeMillis
+  val answer1: Int = ???
+  println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
+
+  val start2: Long = System.currentTimeMillis
+  val answer2: Int = ???
+  println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
