fe9613019ce98463d94a31090e7f0765e94722ce
nmcb <nmcb@xs4all.nl>
2023-12-13 13:22:04 +0100

    incorporated defect count - sim642

--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -6,24 +6,12 @@ object Day13 extends App:
 
   val day: String = this.getClass.getName.drop(3).init
 
-  case class Grid(underlying: Vector[Vector[Char]], smudge: Option[(Int,Int)]):
-    lazy val sizeX: Int = underlying.map(_.size).max
-    lazy val sizeY: Int = underlying.size
-
-    lazy val image: Vector[Vector[Char]] =
-      (for {
-        y <- 0 until sizeY
-      } yield (for {
-        x <- 0 until sizeX
-      } yield
-          smudge match
-            case Some((sx, sy)) if sx == x && sy == y => if underlying(y)(x) == '.' then '#' else '.'
-            case _                                    => underlying(y)(x))
-        .toVector)
-        .toVector
+  case class Grid(image: Vector[Vector[Char]], defects: Int = 0):
+    lazy val sizeX: Int = image.map(_.size).max
+    lazy val sizeY: Int = image.size
 
     override def toString: String =
-      underlying.map(_.mkString).mkString("\n")
+      image.map(_.mkString).mkString("\n")
 
     def mirrorLinesAt(x: Int)(line: Vector[Char]): (Vector[Char], Vector[Char]) =
       val (left, right) = line.splitAt(x)
@@ -34,40 +22,22 @@ object Day13 extends App:
 
 
     def mirrorLine(x: Int): Boolean =
-      image.map(mirrorLinesAt(x)).forall(_ == _)
+      image.map(mirrorLinesAt(x)).count(_ != _) == defects
 
     lazy val mirrorsX: Vector[Int] =
       (1 until sizeX).filter(mirrorLine).toVector
 
     lazy val mirrorsY: Vector[Int] =
-      Grid(underlying.transpose, smudge.map(_.swap)).mirrorsX
+      Grid(image.transpose, defects).mirrorsX
 
     lazy val summarize: Int =
       val col = mirrorsX.headOption.getOrElse(0)
       val row = mirrorsY.headOption.getOrElse(0)
       col + (100 * row)
 
-    lazy val summarizeCorrected: Int =
-      val corrected: Grid =
-        (for {
-          y <- 0 until sizeY
-          x <- 0 until sizeX
-        } yield Grid(underlying, Some((x, y))))
-          .find: (option: Grid) =>
-            val col = option.mirrorsX.diff(mirrorsX)
-            val row = option.mirrorsY.diff(mirrorsY)
-            col.nonEmpty || row.nonEmpty
-          .getOrElse:
-            sys.error("not found")
-
-      val col = corrected.mirrorsX.diff(mirrorsX).headOption.getOrElse(0)
-      val row = corrected.mirrorsY.diff(mirrorsY).headOption.getOrElse(0)
-
-      col + (100 * row)
-
   object Grid:
     def fromString(ss: Array[String]): Grid =
-      Grid(ss.map(_.toVector).toVector, None)
+      Grid(ss.map(_.toVector).toVector, 0)
 
   lazy val grids: Vector[Grid] =
     Source
@@ -83,5 +53,5 @@ object Day13 extends App:
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
   val start2: Long  = System.currentTimeMillis
-  val answer2: Long = grids.map(_.summarizeCorrected).sum
+  val answer2: Long = grids.map(_.copy(defects = 1)).map(_.summarize).sum
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
