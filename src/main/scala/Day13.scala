950cc2b0f10db7c335d83e61349330264389144a
nmcb <nmcb@xs4all.nl>
2023-12-13 21:32:18 +0100

    yak shaving day 13

--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -6,38 +6,32 @@ object Day13 extends App:
 
   val day: String = this.getClass.getName.drop(3).init
 
-  case class Grid(image: Vector[String], defects: Int = 0):
-    lazy val sizeX: Int = image.map(_.size).max
-    lazy val sizeY: Int = image.size
+  case class Grid(image: Vector[Vector[Char]], defects: Int = 0):
 
     def withDefects(nr: Int): Grid =
       copy(defects = nr)
 
-    override def toString: String =
-      image.map(_.mkString).mkString("\n")
-
-    def mirrorLinesAt(n: Int)(line: String): (String, String) =
-      val (left, right) = line.splitAt(n)
-      val length        = left.length min right.length
-      val toTheLeft     = left.reverse.take(length)
-      val toTheRight    = right.take(length)
-      (toTheLeft, toTheRight)
-
-    def hasMirrorLineAt(n: Int): Boolean =
-      image.map(mirrorLinesAt(n)).count(_ != _) == defects
+    lazy val columns: Int =
+      image.map(_.size).max
 
     lazy val mirrorX: Int =
-      (1 until sizeX).find(hasMirrorLineAt).getOrElse(0)
+      (1 until columns).find: n =>
+        image.count: line =>
+          val (l, r) = line splitAt n
+          val length = l.length min r.length
+          l.reverse.take(length) != r.take(length)
+        == defects
+      .getOrElse(0)
 
     lazy val mirrorY: Int =
-      copy(image = image.map(_.toVector).transpose.map(_.mkString)).mirrorX
+      copy(image = image.transpose).mirrorX
 
     lazy val summarize: Int =
       mirrorX + (100 * mirrorY)
 
   object Grid:
     def fromString(ss: Array[String]): Grid =
-      Grid(ss.toVector)
+      Grid(ss.map(_.toVector).toVector)
 
   lazy val grids: Vector[Grid] =
     Source
