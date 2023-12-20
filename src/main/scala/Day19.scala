5997bbbe31fc3c457861913efb3dd9bb7e875e06
nmcb <nmcb@xs4all.nl>
2023-12-20 06:39:16 +0100

    baselined day 20

--- src/main/scala/Day19.scala
+++ src/main/scala/Day19.scala
@@ -15,9 +15,11 @@ object Day19 extends App:
 
   import Rule.*
 
-  case class Range(from: Long, to: Long)
+  case class Range(from: Long, to: Long):
+    def size: Long = to - from + 1
 
-  case class Search(x: Range, m: Range, a: Range, s: Range, workflow: String)
+  case class Search(x: Range, m: Range, a: Range, s: Range, workflow: String):
+    def size: Long = x.size * m.size * a.size * s.size
 
   type Workflows = Map[String, List[Rule]]
 
@@ -30,10 +32,11 @@ object Day19 extends App:
             .map: s =>
               if s.contains('<') || s.contains('>') then
                 val selector: Part => Long =
-                  if      s.startsWith("x") then _.x
-                  else if s.startsWith("m") then _.m
-                  else if s.startsWith("a") then _.a
-                  else                           _.s
+                  s match
+                    case s"x$rest" => _.x
+                    case s"m$rest" => _.m
+                    case s"a$rest" => _.a
+                    case s"s$rest" => _.s
                 val valueStr = s.drop(2).takeWhile(_.isDigit)
                 val value    = valueStr.toLong
                 val workflow = s.drop(3 + valueStr.length)
@@ -69,96 +72,88 @@ object Day19 extends App:
   val (parts, workflows) = parse(lines)
 
 
-  def solve1(): Long =
-    def loop(part: Part, workflow: String, workflows: Workflows): Rule =
-      val rules = workflows(workflow)
-      val matched =
-        rules
-          .find:
-            case Accepted                         => true
-            case Rejected                         => true
-            case Deferred(workflow)               => true
-            case Compared(select, _, workflow, _) => select(part)
-          .getOrElse(sys.error(s"no rule matched: $workflow"))
+  def process1(part: Part, workflow: String, workflows: Workflows): Rule =
+    val rules = workflows(workflow)
+    val matched =
+      rules
+        .find:
+          case Accepted                  => true
+          case Rejected                  => true
+          case Deferred(_)               => true
+          case Compared(select, _, _, _) => select(part)
+        .getOrElse(sys.error(s"no rule matched: $workflow"))
 
-      matched match
-        case Accepted                    => Accepted
-        case Rejected                    => Rejected
-        case Deferred(workflow)          => loop(part, workflow, workflows)
-        case Compared(_, _, workflow, _) => loop(part, workflow, workflows)
+    matched match
+      case Accepted                    => Accepted
+      case Rejected                    => Rejected
+      case Deferred(workflow)          => process1(part, workflow, workflows)
+      case Compared(_, _, workflow, _) => process1(part, workflow, workflows)
 
 
+
+  val start1: Long  = System.currentTimeMillis
+  val answer1: Long =
     parts
-      .filter(part => loop(part, "in", workflows) == Accepted)
+      .filter(part => process1(part, "in", workflows) == Accepted)
       .map(p => p.x + p.m + p.a + p.s)
       .sum
 
-  val start1: Long  = System.currentTimeMillis
-  val answer1: Long = solve1()
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
-  def solve2(): Long =
-    def loop(searches: List[Search], workflows: Workflows, found: List[Search]): List[Search] =
-      if searches.isEmpty then
-        found
-      else
-        val search = searches.head
-        val rules = workflows(search.workflow)
+  def process2(searches: List[Search], workflows: Workflows, found: List[Search]): List[Search] =
+    if searches.isEmpty then
+      found
+    else
+      val search = searches.head
+      val rules = workflows(search.workflow)
 
-        def selector(char: Char): Search => Range =
-          if      char == 'x' then _.x
-          else if char == 'm' then _.m
-          else if char == 'a' then _.a
-          else _.s
+      def selector(char: Char): Search => Range =
+        char match
+          case 'x' => _.x
+          case 'm' => _.m
+          case 'a' => _.a
+          case 's' => _.s
 
-        val matched =
-          rules.find:
-            case Accepted => true
-            case Rejected => true
-            case Deferred(workflow) => true
-            case Compared(_, select, workflow, char) =>
-              val min = selector(char)(search).from
-              val max = selector(char)(search).to
-              (min to max).exists(select)
-          .getOrElse(sys.error(s"no rule matched"))
-
-        matched match
-          case Accepted => loop(searches.tail, workflows, search +: found)
-          case Rejected => loop(searches.tail, workflows, found)
-          case Deferred(workflow) => loop(search.copy(workflow = workflow) +: searches.tail, workflows, found)
+      val matched =
+        rules.find:
+          case Accepted => true
+          case Rejected => true
+          case Deferred(workflow) => true
           case Compared(_, select, workflow, char) =>
-            val min = selector(char)(search).from
-            val max = selector(char)(search).to
-            val included = (min to max).map(select)
-            val split = (1 until included.size).find(i => included(i - 1) != included(i))
+            val range = selector(char)(search)
+            (range.from to range.to).exists(select)
+        .getOrElse(sys.error(s"no rule matched"))
 
-            split match
-              case None => loop(search.copy(workflow = workflow) +: searches.tail, workflows, found)
-              case Some(at) =>
+      matched match
+        case Accepted => process2(searches.tail, workflows, search :: found)
+        case Rejected => process2(searches.tail, workflows, found)
+        case Deferred(workflow) => process2(search.copy(workflow = workflow) :: searches.tail, workflows, found)
+        case Compared(_, select, workflow, char) =>
+          val min = selector(char)(search).from
+          val max = selector(char)(search).to
+          val included = (min to max).map(select)
+          val split = (1 until included.size).find(i => included(i - 1) != included(i))
 
-                def make(range: Range): List[Range] =
-                  List(Range(range.from, range.from + at - 1), Range(range.from + at, range.to))
+          split match
+            case None     => process2(search.copy(workflow = workflow) :: searches.tail, workflows, found)
+            case Some(at) =>
 
-                val splits =
-                  if      char == 'x' then make(search.x).map(r => search.copy(x = r))
-                  else if char == 'm' then make(search.m).map(r => search.copy(m = r))
-                  else if char == 'a' then make(search.a).map(r => search.copy(a = r))
-                  else                     make(search.s).map(r => search.copy(s = r))
+              def make(range: Range): List[Range] =
+                List(Range(range.from, range.from + at - 1), Range(range.from + at, range.to))
 
-                loop(splits ++ searches.tail, workflows, found)
+              val splits =
+                char match
+                  case 'x' => make(search.x).map(r => search.copy(x = r))
+                  case 'm' => make(search.m).map(r => search.copy(m = r))
+                  case 'a' => make(search.a).map(r => search.copy(a = r))
+                  case 's' => make(search.s).map(r => search.copy(s = r))
 
-    val start = Search(Range(1, 4000), Range(1, 4000), Range(1, 4000), Range(1, 4000), "in")
-    val ranges = loop(List(start), workflows, List.empty)
+              process2(splits ++ searches.tail, workflows, found)
 
-    ranges
-      .map: r =>
-        val x = r.x.to - r.x.from + 1
-        val m = r.m.to - r.m.from + 1
-        val a = r.a.to - r.a.from + 1
-        val s = r.s.to - r.s.from + 1
-        x * m * a * s
-      .sum
 
   val start2: Long  = System.currentTimeMillis
-  val answer2: Long = solve2()
+  val answer2: Long =
+    val start = Search(Range(1, 4000), Range(1, 4000), Range(1, 4000), Range(1, 4000), "in")
+    val ranges = process2(List(start), workflows, List.empty)
+    ranges.map(_.size).sum
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
