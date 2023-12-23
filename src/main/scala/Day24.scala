a20720625f09727d04f241fa6a63bb626a830e50
nmcb <nmcb@xs4all.nl>
2023-12-23 20:34:31 +0100

    baselined day 24

---
+++ src/main/scala/Day24.scala
@@ -0,0 +1,22 @@
+import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.io.Source
+
+object Day24 extends App:
+
+  val day: String =
+    this.getClass.getName.drop(3).init
+
+  val input: Vector[String] =
+    Source
+      .fromResource(s"input$day.txt")
+      .getLines
+      .toVector
+
+  val start1: Long = System.currentTimeMillis
+  val answer1: Int = ???
+  println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
+
+  val start2: Long  = System.currentTimeMillis
+  val answer2: Int = ???
+  println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
