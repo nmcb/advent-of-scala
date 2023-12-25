b8970112f57c6df71a86885fdb0b2731d79e8c05
nmcb <nmcb@xs4all.nl>
2023-12-25 01:18:21 +0100

    baselining

--- src/main/scala/Day25.scala
+++ src/main/scala/Day25.scala
@@ -14,9 +14,9 @@ object Day25 extends App:
       .toVector
 
   val start1: Long = System.currentTimeMillis
-  val answer1: Int = 666
+  val answer1: Int = ???
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
   val start2: Long  = System.currentTimeMillis
-  val answer2: Int = 666
+  val answer2: Int = ???
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
