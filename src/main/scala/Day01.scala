3cee276cfa29f07ee5b3c71e663dcc095f5ed439
nmcb <nmcb@xs4all.nl>
2024-12-01 07:07:14 +0100

    solved day 01

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -5,18 +5,34 @@ object Day01 extends App:
   val day: String =
     this.getClass.getName.drop(3).init
 
-  val values: List[String] =
+  val values: List[(Int,Int)] =
     Source
       .fromResource(s"input$day.txt")
       .getLines
+      .map:
+        case s"$l   $r" => (l.toInt, r.toInt)
       .toList
+
+  def distance(a: Int, b: Int): Int =
+    if a >= b then a - b else b - a
 
   val start1: Long =
     System.currentTimeMillis
 
   val answer1: Int =
-    666
+    val left  = values.map(_._1).sorted
+    val right = values.map(_._2).sorted
+    left.zip(right).map(distance).sum
 
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
+  val start2: Long =
+    System.currentTimeMillis
+
+  val answer2: Int =
+    val left  = values.map(_._1).sorted
+    val right = values.map(_._2).sorted
+    left.foldLeft(0)((a,l) => a + right.count(_ == l) * l)
+
+  println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
 
--- src/test/scala/TestSolutions.scala
+++ src/test/scala/TestSolutions.scala
@@ -2,5 +2,6 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestSolutions extends AnyFunSuite:
   test("Day01") {
-    assertResult(666)(actual = Day01.answer1)
+    assertResult( 2057374)(actual = Day01.answer1)
+    assertResult(23177084)(actual = Day01.answer2)
   }
