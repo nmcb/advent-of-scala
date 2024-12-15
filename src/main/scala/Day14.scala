d9b51005baf5925e4b5f417e5e135accf5fe3d7e
nmcb <nmcb@xs4all.nl>
2024-12-15 23:05:04 +0100

    performance and cleanup

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -11,14 +11,21 @@ object Day14 extends App:
     infix def +(that: Pos): Pos =
       Pos(x + that.x, y + that.y)
 
-    def neighbourOf(p: Pos): Boolean =
-      List(copy(x = x - 1), copy(x = x + 1), copy(y = y - 1), copy(y = y +1)).contains(p)
+    lazy val neighbours: Set[Pos] =
+      Set(copy(x = x - 1), copy(x = x + 1), copy(y = y - 1), copy(y = y + 1))
+
+    inline def neighbourOf(p: Pos): Boolean =
+      neighbours.contains(p)
 
   case class Robot(p: Pos, v: Pos)
 
-  val input: Vector[Robot] =
-    Source.fromResource(s"input$day.txt").getLines.toVector.map:
-      case s"p=$px,$py v=$vx,$vy" => Robot(Pos(px.toInt, py.toInt), Pos(vx.toInt, vy.toInt))
+  val space: Space =
+    val robots: Vector[Robot] =
+      Source.fromResource(s"input$day.txt").getLines.toVector.map:
+        case s"p=$px,$py v=$vx,$vy" =>
+          Robot(Pos(px.toInt, py.toInt), Pos(vx.toInt, vy.toInt))
+
+    Space(robots, sizeX = 101, sizeY = 103)
 
   case class Space(robots: Vector[Robot], sizeX: Int, sizeY: Int):
 
@@ -58,30 +65,30 @@ object Day14 extends App:
     def safetyFactor: Long =
       List(robotsInQ1, robotsInQ2, robotsInQ3, robotsInQ4).map(_.size.toLong).product
 
+    def connected: Set[Set[Pos]] =
+      @tailrec
+      def loop(todo: Vector[Pos], result: Set[Set[Pos]]): Set[Set[Pos]] =
+        if todo.isEmpty then
+          result
+        else
+          val remove = result.filter(_.exists(_.neighbourOf(todo.head)))
+          val add = remove.foldLeft(Set.empty[Pos])(_ ++ _) + todo.head
+          loop(todo.tail, result = result -- remove + add)
+
+      val todo = robotsByPos.keys.toVector
+      val start = Set(Set(todo.head))
+      loop(todo.tail, start)
+
   val start1: Long  = System.currentTimeMillis
-  val answer1: Long = (0 until 100).foldLeft(Space(input, sizeX = 101, sizeY = 103))((s,_) => s.next).safetyFactor
+  val answer1: Long = (0 until 100).foldLeft(space)((s,_) => s.next).safetyFactor
   println(s"Answer day $day part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
-
-  def connected(space: Space): Set[Set[Pos]] =
-    @tailrec
-    def loop(todo: Vector[Pos], result: Set[Set[Pos]]): Set[Set[Pos]] =
-      if todo.isEmpty then
-        result
-      else
-        val remove = result.filter(_.exists(_.neighbourOf(todo.head)))
-        val add    = remove.foldLeft(Set.empty[Pos])(_ ++ _) + todo.head
-        loop(todo.tail, result = result -- remove + add)
-
-    val todo  = space.robotsByPos.keys.toVector
-    val start = Set(Set(todo.head))
-    loop(todo.tail, start)
 
   val start2: Long = System.currentTimeMillis
   val answer2: Int =
     val (_, iterations) = Iterator
-      .iterate(Space(input, sizeX = 101, sizeY = 103))(_.next)
+      .iterate(space)(_.next)
       .zipWithIndex
-      .find((s,t) => connected(s).maxBy(_.size).size > 50)
+      .find((s,_) => s.connected.maxBy(_.size).size > 50)
       .get
     iterations
 
