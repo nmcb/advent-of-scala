1a96761b10330cdb43611e0b7450a4877af4981f
nmcb <nmcb@xs4all.nl>
2024-12-18 03:02:50 +0100

    cleanup

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -9,68 +9,43 @@ object Day17 extends App:
   val day: String =
     getClass.getName.drop(3).init
 
-  sealed abstract class Inst(cpu: CPU):
-    def operand: Long
-    def perform: CPU
-
-    def literal: Long =
-      operand
-
-    def combo: Long =
-      operand match
-        case 0 | 1 | 2 | 3 => operand
-        case 4 => cpu.a
-        case 5 => cpu.b
-        case 6 => cpu.c
-        case _ => sys.error(s"Invalid combo operand: $operand")
-
-  case class ADV(override val operand: Long, cpu: CPU) extends Inst(cpu):
-    def perform: CPU = cpu.copy(a = cpu.a >> combo, ip = cpu.ip + 2)
-
-  case class BXL(override val operand: Long, cpu: CPU) extends Inst(cpu):
-    def perform: CPU = cpu.copy(b = cpu.b ^ literal, ip = cpu.ip + 2)
-
-  case class BST(override val operand: Long, cpu: CPU) extends Inst(cpu):
-    def perform: CPU = cpu.copy(b = combo & 0x07, ip = cpu.ip + 2)
-
-  case class JNZ(override val operand: Long, cpu: CPU) extends Inst(cpu):
-    def perform: CPU = if cpu.a != 0 then cpu.copy(ip = literal.toInt) else cpu.copy(ip = cpu.ip + 2)
-
-  case class BXC(override val operand: Long, cpu: CPU) extends Inst(cpu):
-    def perform: CPU = cpu.copy(b = cpu.b ^ cpu.c, ip = cpu.ip + 2)
-
-  case class OUT(override val operand: Long, cpu: CPU) extends Inst(cpu):
-    def perform: CPU = cpu.copy(out = (cpu.out << 3) + (combo & 0x07), ip = cpu.ip + 2)
-
-  case class BDV(override val operand: Long, cpu: CPU) extends Inst(cpu):
-    def perform: CPU = cpu.copy(b = cpu.a >> combo, ip = cpu.ip + 2)
-
-  case class CDV(override val operand: Long, cpu: CPU) extends Inst(cpu):
-    def perform: CPU = cpu.copy(c = cpu.a >> combo, ip = cpu.ip + 2)
-
-
   case class CPU(a: Long, b: Long, c: Long, program: Vector[Long], ip: Int = 0, out: Long = 0):
-    lazy val opcode  = program(ip)
-    lazy val operand = program(ip + 1)
 
-    val halts: Boolean =
+    def opcode =
+      program(ip)
+
+    def operand =
+      program(ip + 1)
+
+    def halts: Boolean =
       ip >= program.size
 
     @tailrec
     final def run: CPU =
-      if halts then this else read.perform.run
+      if halts then this else read.run
 
-    def read: Inst =
+    def combo: Long =
+      operand match
+        case 0 | 1 | 2 | 3 => operand
+        case 4 => a
+        case 5 => b
+        case 6 => c
+        case _ => sys.error(s"Invalid combo operand: $operand")
+
+    def literal: Long =
+      operand
+
+    def read: CPU =
       opcode match
-        case 0 => ADV(operand, this)
-        case 1 => BXL(operand, this)
-        case 2 => BST(operand, this)
-        case 3 => JNZ(operand, this)
-        case 4 => BXC(operand, this)
-        case 5 => OUT(operand, this)
-        case 6 => BDV(operand, this)
-        case 7 => CDV(operand, this)
-        case _ => sys.error(s"Invalid read: ip=$ip, inst=${program(ip)}")
+        case 0 => copy(a = a >> combo, ip = ip + 2)
+        case 1 => copy(b = b ^ literal, ip = ip + 2)
+        case 2 => copy(b = combo & 0x07, ip = ip + 2)
+        case 3 => copy(ip = if a != 0 then literal.toInt else ip + 2)
+        case 4 => copy(b = b ^ c, ip = ip + 2)
+        case 5 => copy(out = (out << 3) + (combo & 0x07), ip = ip + 2)
+        case 6 => copy(b = a >> combo, ip = ip + 2)
+        case 7 => copy(c = a >> combo, ip = ip + 2)
+        case _ => sys.error(s"Invalid r.performead: ip=$ip, inst=${program(ip)}")
 
     @tailrec
     final def display(current: Long, result: String = ""): String =
