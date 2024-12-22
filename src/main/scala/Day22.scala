9eabd1c7ac7ec7e9f20a537278344d4787a2a39b
nmcb <nmcb@xs4all.nl>
2024-12-22 15:30:03 +0100

    solved day 22 - part 2

--- src/main/scala/Day22.scala
+++ src/main/scala/Day22.scala
@@ -26,59 +26,38 @@ object Day22 extends App:
       pruned
 
     def next: Secret =
-      val stage1 = mul(64, number)
-      val stage2 = div(32, stage1)
+      val stage1 = mul(  64, number)
+      val stage2 = div(  32, stage1)
       val stage3 = mul(2048, stage2)
 
       Secret(stage3)
 
-  extension (l: Long)
-    def bin: String = l.toBinaryString.padTo(64 - l.toString.length, '0')
-
   val start1: Long  = System.currentTimeMillis
   val answer1: Long = input.map(initial => Iterator.range(0, 2000).foldLeft(initial)((l,_) => l.next)).map(_.number).sum
-
   println(s"Answer day $day part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
 
-  case class Buyer(initial: Secret):
-    val secrets = Vector.range(0, 1999).foldLeft(Vector(initial))((a,_) => a :+ a.last.next)
-    val prices  = secrets.map(_.number.toString.last.asDigit)
-    val diffs   = 0 +: prices.sliding(2).map(d => d(1) - d(0)).toVector
+  extension (p: (Seq[Long],Long))
+    def sequence: Seq[Long] = p._1
+    def price: Long         = p._2
 
-    val highestPrice          = prices.drop(5).max
-    val highestPriceIndexes   = prices.zipWithIndex.drop(5).filter(_._1 == highestPrice).map(_._2)
+  def solve(secrets: Vector[Secret]): Long =
+    secrets
+      .flatMap: initial =>
+        Iterator.iterate(initial, 2001)(_.next)
+          .map(_.number % 10)
+          .sliding(5)
+          .map: prices =>
+            prices
+              .zip(prices.tail)
+              .map((a,b) => b - a)
+              -> prices.last
+          .toVector
+          .groupMap(_.sequence)(_.price)
+          .map(_ -> _.head)
+      .groupMapReduce(_.sequence)(_.price)(_ + _)
+      .values
+      .max
 
-    val priceWithIndexToPriceSequence =
-      (0 to 9).foldLeft(Vector.empty[((Int,Int),Vector[Int])]): (acc,price) =>
-        val priceIndexes = prices.zipWithIndex.drop(5).filter(_._1 == price).map(_._2)
-
-        priceIndexes.foldLeft(acc): (acc,idx) =>
-          acc :+ (prices(idx) -> idx) -> diffs.slice(idx - 3, idx + 1)
-
-    def pricesWithIndexesBy(sequence: Vector[Int]): Vector[(Int,Int)] =
-      diffs.sliding(4)
-        .zipWithIndex
-        .map((seq,idx) => prices(idx + 3) -> (idx + 3) -> seq)
-        .filter(_._2 == sequence)
-        .map(_._1)
-        .toVector
-
-
-  val buyers = input.map(Buyer.apply)
-//  println(buyers.map(_.diffs.mkString("\n")))
-//  println(buyers.map(_.highestPrice))
-//  println(buyers.map(_.highestPriceIndexes))
-
-
-  val x = buyers.map(_.pricesWithIndexesBy(Vector(-2,1,-1,3)))
-  println(x)
-
-  val y = buyers.map(b => b.priceWithIndexToPriceSequence.)
-//  println(y)
-
-
-
-
-  val start2: Long = System.currentTimeMillis
-  val answer2: Long = 666
+  val start2: Long  = System.currentTimeMillis
+  val answer2: Long = solve(input)
   println(s"Answer day $day part 2: $answer2 [${System.currentTimeMillis - start2}ms]")
