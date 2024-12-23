bf0d929a3373513eda3e0e5d2c63132a1414792a
nmcb <nmcb@xs4all.nl>
2024-12-23 14:51:54 +0100

    grammar

--- src/main/scala/nmcb/BronKerbosch.scala
+++ src/main/scala/nmcb/BronKerbosch.scala
@@ -3,22 +3,22 @@ package nmcb
 object BronKerbosch:
 
   /** Computes the maximum clique, i.e. the largest group of neighbouring nodes. */
-  def run[A](neighbors: Map[A,Set[A]]): Set[A] =
+  def run[A](neighbours: Map[A,Set[A]]): Set[A] =
     var maximum: Set[A] = Set.empty
 
     def loop(r: Set[A], p: Set[A], x: Set[A]): Unit =
       if p.isEmpty && x.isEmpty then
         if r.size > maximum.size then maximum = r
       else
-        val u  = (p union x).maxBy(neighbors(_).size)
+        val u  = (p union x).maxBy(neighbours(_).size)
         var p2 = p
         var x2 = x
         for
-          v <- p diff neighbors(u)
+          v <- p diff neighbours(u)
         yield
-          loop(r + v, p2 intersect neighbors(v), x2 intersect neighbors(v))
+          loop(r + v, p2 intersect neighbours(v), x2 intersect neighbours(v))
           p2 -= v
           x2 += v
 
-    loop(Set.empty, neighbors.keySet, Set.empty)
+    loop(Set.empty, neighbours.keySet, Set.empty)
     maximum
