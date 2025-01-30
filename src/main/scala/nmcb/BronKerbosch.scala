fb956ba34e28a103ef7e2d2feb9820baa391a632
nmcb <nmcb@xs4all.nl>
2025-01-30 12:07:48 +0100

    documented bron-kerbosch implementation

--- src/main/scala/nmcb/BronKerbosch.scala
+++ src/main/scala/nmcb/BronKerbosch.scala
@@ -10,15 +10,18 @@ object BronKerbosch:
       if p.isEmpty && x.isEmpty then
         if r.size > maximum.size then maximum = r
       else
+        // Technically this implements Tomita's optimisation which recurses with a pivot node
+        // chosen to contain the maximum number of edges. This variant of the Bron-Kerbosch
+        // algorithm performs up to twice as fast on sparse, and similar on dense graphs.
         val u  = (p union x).maxBy(neighbours(_).size)
-        var p2 = p
-        var x2 = x
+        var bp = p
+        var bx = x
         for
           v <- p diff neighbours(u)
         yield
           loop(r + v, p intersect neighbours(v), x intersect neighbours(v))
-          p2 -= v
-          x2 += v
+          bp -= v
+          bx += v
 
     loop(Set.empty, neighbours.keySet, Set.empty)
     maximum
