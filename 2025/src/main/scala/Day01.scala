4fdbeba444180245897f4a56485612e38c1aa83b
nmc.borst <nmcb@xs4all.nl>
2025-12-01 07:15:12 +0100

    202501 (#34)
    * baselining
    
    * fixed 2024 day 07 test
    
    * fixed day 07
    
    * solved 2025 day 01

--- 2025/src/main/scala/Day01.scala
+++ 2025/src/main/scala/Day01.scala
@@ -4,18 +4,39 @@ object Day01 extends App:
 
   val day = getClass.getSimpleName.filter(_.isDigit).mkString
 
-  val numbers: Seq[Int] =
+  val input: Vector[String] =
       Source
         .fromResource(s"input$day.txt")
         .getLines
-        .map:
-          case s"$num" => num.toInt
-        .toSeq
+        .toVector
+
+  extension (current: Int)
+    infix def next(step: Char): Int =
+      step match
+        case 'L' if current == 0 => 99
+        case 'L' => current - 1
+        case 'R' if current == 99 => 0
+        case 'R' => current + 1
+
+  def solve1(input: Vector[String], start: Vector[Int] = Vector(50)) =
+    input.foldLeft(start): (trace, rotation) =>
+      rotation match
+        case s"R$count" => trace :+ List.fill(count.toInt)('R').foldLeft(trace.last)(_ next _)
+        case s"L$count" => trace :+ List.fill(count.toInt)('L').foldLeft(trace.last)(_ next _)
 
   lazy val start1: Long = System.currentTimeMillis
-  lazy val answer1: Int = numbers.min
+  lazy val answer1: Int = solve1(input).count(_ == 0)
   println(s"Answer AOC 2024 day $day part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
 
+  def solve2(input: Vector[String], start: Vector[Int] = Vector(50)) =
+    input
+      .foldLeft(""): (result, rotation) =>
+        rotation match
+          case s"R$count" => result + ("R" * count.toInt)
+          case s"L$count" => result + ("L" * count.toInt)
+      .foldLeft(start): (trace, step) =>
+        trace :+ trace.last.next(step)
+
   lazy val start2: Long = System.currentTimeMillis
-  lazy val answer2: Int = numbers.max
+  lazy val answer2: Int = solve2(input).count(_ == 0)
   println(s"Answer AOC 2024 day $day part 2: $answer2 [${System.currentTimeMillis - start2}ms]")
--- 2025/src/test/scala/TestDay01.scala
+++ 2025/src/test/scala/TestDay01.scala
@@ -2,7 +2,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestDay01 extends AnyFunSuite:
   test("Day01") {
-    assertResult(1)(Day01.answer1)
-    assertResult(5)(Day01.answer2)
+    assertResult(1055)(Day01.answer1)
+    assertResult(6386)(Day01.answer2)
   }
   
