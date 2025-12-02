c154816e541595bc222ac4f0aa46c3f4cdf660e0
nmc.borst <nmcb@xs4all.nl>
2025-12-02 07:24:27 +0100

    solved day 02; performance day 01 (#35)

--- 2025/src/main/scala/Day01.scala
+++ 2025/src/main/scala/Day01.scala
@@ -10,33 +10,36 @@ object Day01 extends App:
         .getLines
         .toVector
 
-  extension (current: Int)
-    infix def next(step: Char): Int =
-      step match
-        case 'L' if current == 0 => 99
-        case 'L' => current - 1
-        case 'R' if current == 99 => 0
-        case 'R' => current + 1
+//  extension (current: Int)
+//    infix def next(step: Char): Int =
+//      step match
+//        case 'L' if current == 0 => 99
+//        case 'L' => current - 1
+//        case 'R' if current == 99 => 0
+//        case 'R' => current + 1
 
-  def solve1(input: Vector[String], start: Vector[Int] = Vector(50)) =
-    input.foldLeft(start): (trace, rotation) =>
-      rotation match
-        case s"R$count" => trace :+ List.fill(count.toInt)('R').foldLeft(trace.last)(_ next _)
-        case s"L$count" => trace :+ List.fill(count.toInt)('L').foldLeft(trace.last)(_ next _)
+  def solve1(input: Vector[String]) =
+    input
+      .scanLeft(50): (last, rotation) =>
+        rotation match
+          case s"R$count" => last + count.toInt
+          case s"L$count" => last - count.toInt
+      .count(step => step % 100 == 0)
 
-  lazy val start1: Long = System.currentTimeMillis
-  lazy val answer1: Int = solve1(input).count(_ == 0)
+  val start1: Long = System.currentTimeMillis
+  lazy val answer1: Int = solve1(input)
   println(s"Answer AOC 2024 day $day part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
 
-  def solve2(input: Vector[String], start: Vector[Int] = Vector(50)) =
-    input
-      .foldLeft(""): (result, rotation) =>
-        rotation match
-          case s"R$count" => result + ("R" * count.toInt)
-          case s"L$count" => result + ("L" * count.toInt)
-      .foldLeft(start): (trace, step) =>
-        trace :+ trace.last.next(step)
 
-  lazy val start2: Long = System.currentTimeMillis
-  lazy val answer2: Int = solve2(input).count(_ == 0)
+  def solve2(input: Vector[String]) =
+    input
+      .flatMap:
+          case s"R$count" => Vector.fill(count.toInt)(1)
+          case s"L$count" => Vector.fill(count.toInt)(-1)
+          case code       => sys.error(s"invalid code: $code")
+      .scanLeft(50)(_ + _)
+      .count(step => step % 100 == 0)
+
+  val start2: Long = System.currentTimeMillis
+  lazy val answer2: Int = solve2(input)
   println(s"Answer AOC 2024 day $day part 2: $answer2 [${System.currentTimeMillis - start2}ms]")
